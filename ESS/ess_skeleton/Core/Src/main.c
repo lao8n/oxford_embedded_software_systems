e44712f4c937d3e1ea0847fd60257ee480a3e1aa
Nicholas Drake <hellolao8n@gmail.com>
2024-01-23 09:26:57 +0000

    code runs successfully finally

--- ESS/ess_skeleton/Core/Src/main.c
+++ ESS/ess_skeleton/Core/Src/main.c
@@ -2,6 +2,24 @@
 #include "main.h"
 #include "ess_helper.h"
 
+// Initialize the green LED.
+void led_green_init(void);
+// Turn the green LED on
+void led_green_on(void);
+// Turn the green LED off
+void led_green_off(void);
+
+
+void led_green_init() {
+	*(uint32_t*)0x40020C14 & 0x1000;
+}
+void led_green_on() {
+	*(uint32_t*)0x40020C14 | 0x1000;
+}
+void led_green_off() {
+	*(uint32_t*)0x40020C14 & 0x1000;
+}
+
 int main(void)
 {
 	/* Initialize system */
@@ -9,27 +27,7 @@ int main(void)
 	/* Initialize peripherals on board */
 	ess_helper_init();
 	// Set all the LEDs to on.
-
-
-	// Initialize the green LED.
-	void led_green_init(void);
-	// Turn the green LED on
-	void led_green_on(void);
-	// Turn the green LED off
-	void led_green_off(void);
-
-
 	*(uint32_t*)0x40020C14 = 0xF000;
-
-	void led_green_init() {
-		*(uint32_t*)0x40020C14 & 0x1000;
-	}
-	void led_green_on() {
-		*(uint32_t*)0x40020C14 | 0x1000;
-	}
-	void led_green_off() {
-		*(uint32_t*)0x40020C14 & 0x1000;
-	}
 
 	led_green_init();
 	led_green_on();
