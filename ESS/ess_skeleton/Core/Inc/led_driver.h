ec130bf1c9efce429d9268ac5334a863300c7094
Nicholas Drake <hellolao8n@gmail.com>
2024-01-23 16:25:25 +0000

    got all 4 fading together though not one after another

--- ESS/ess_skeleton/Core/Inc/led_driver.h
+++ ESS/ess_skeleton/Core/Inc/led_driver.h
@@ -13,6 +13,7 @@ void led_init(LED_t *led, uint32_t * port, uint32_t pin);
 void led_on(LED_t * led);
 void led_off(LED_t * led);
 void delay_msec(uint32_t delay);
+void delay_usec(uint32_t delay){
 
 struct Buttonstruct {
 	uint32_t * port; // input button register
