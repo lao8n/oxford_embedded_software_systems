8dee0b52834419452736fd7252af110ed9f6cec2
Nicholas Drake <hellolao8n@gmail.com>
2024-01-24 16:32:48 +0000

    confirmed that spi get has default value at who am i register of 0x3f

---
+++ ESS/ess_skeleton/Debug/Core/Src/display_driver.d
@@ -0,0 +1,4 @@
+Core/Src/display_driver.o: ../Core/Src/display_driver.c \
+ ../Core/Inc/display_driver.h ../Core/Inc/led_driver.h
+../Core/Inc/display_driver.h:
+../Core/Inc/led_driver.h:
--- ESS/ess_skeleton/Debug/Core/Src/main.d
+++ ESS/ess_skeleton/Debug/Core/Src/main.d
@@ -31,7 +31,8 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/ess_helper.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
  ../Core/Inc/stm32f4xx_it.h ../Core/Inc/pwm_driver.h \
- ../Core/Inc/led_driver.h
+ ../Core/Inc/led_driver.h ../Core/Inc/stm32f4xx_it.h \
+ ../Core/Inc/display_driver.h ../Core/Inc/spi_driver.h
 ../Core/Inc/ess_helper.h:
 ../Core/Inc/led_driver.h:
 ../Core/Inc/delay.h:
@@ -69,3 +70,6 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/ess_helper.h \
 ../Core/Inc/stm32f4xx_it.h:
 ../Core/Inc/pwm_driver.h:
 ../Core/Inc/led_driver.h:
+../Core/Inc/stm32f4xx_it.h:
+../Core/Inc/display_driver.h:
+../Core/Inc/spi_driver.h:
