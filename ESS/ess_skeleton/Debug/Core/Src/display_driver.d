8baef2f1af949e59941d66a99f7c501d2a3b628f
Nicholas Drake <hellolao8n@gmail.com>
2024-01-25 14:21:26 +0000

    lab 3 1 part c working

--- ESS/ess_skeleton/Debug/Core/Src/display_driver.d
+++ ESS/ess_skeleton/Debug/Core/Src/display_driver.d
@@ -1,4 +1,7 @@
 Core/Src/display_driver.o: ../Core/Src/display_driver.c \
- ../Core/Inc/display_driver.h ../Core/Inc/led_driver.h
+ ../Core/Inc/display_driver.h ../Core/Inc/led_driver.h \
+ ../Core/Inc/pwm_driver.h ../Core/Inc/led_driver.h
 ../Core/Inc/display_driver.h:
+../Core/Inc/led_driver.h:
+../Core/Inc/pwm_driver.h:
 ../Core/Inc/led_driver.h:
