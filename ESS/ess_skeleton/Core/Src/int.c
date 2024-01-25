a63aa01a35d66b17aaeb6c95ee3cbbcaa182d254
Nicholas Drake <hellolao8n@gmail.com>
2024-01-25 15:30:40 +0000

    red green positive negative tilt fade working

--- ESS/ess_skeleton/Core/Src/int.c
+++ ESS/ess_skeleton/Core/Src/int.c
@@ -1,4 +1,5 @@
 #include "stm32f4xx_hal.h"
+#include "display_driver.h"
 
 TIM_HandleTypeDef TIM_Handle;
 
--- ESS/ess_skeleton/Core/Src/main.c
+++ ESS/ess_skeleton/Core/Src/main.c
@@ -42,25 +42,48 @@ int main(void){
 	pwm_driver_init(&led_green, &led_red, &led_orange, &led_blue);
 
 	// set brightness values
-	pwm_driver_set(0, 0);
-	pwm_driver_set(1, 25);
-	pwm_driver_set(2, 50);
-	pwm_driver_set(3, 100);
-
-	TMR4_Init_ISR();
-	TIM4_IRQHandler();
+	pwm_driver_set(GREEN, 0);
+	pwm_driver_set(ORANGE, 0);
+	pwm_driver_set(BLUE, 0);
+	pwm_driver_set(RED, 0);
 
 	SPIAcc_Init();
+	TMR4_Init_ISR();
+
 	uint8_t buffer[1];
 	SPIAcc_Get(0x0F, buffer, 1); // WHOAMI
 	// set fastest sampling rate
 	buffer[1] = 0x87;
 	SPIAcc_Send(20, buffer, 1); // CTRL_REG1
-	uint8_t data_h[1];
-	SPIAcc_Get(28, data_h, 1); // OUT_X_H
-	uint8_t data_l[1];
-	SPIAcc_Get(29, data_l, 1); // OUT_X_L
-	int16_t x_axis = ((int16_t)data_h << 8) + data_l;
+
 	// loop
-	while(1){};
+	while(1){
+		uint8_t x_data[2];
+		SPIAcc_Get(0x28, x_data, 2); // OUT_X_L, OUT_X_H;
+		int16_t x_axis = (x_data[1] << 8) + x_data[0];
+		if(x_axis < -2000) {
+			pwm_driver_set(GREEN, 100);
+			pwm_driver_set(RED, 0);
+		} else if (x_axis > 2000) {
+			pwm_driver_set(RED, 100);
+			pwm_driver_set(GREEN, 0);
+		} else if (x_axis < -1000){
+			// -1000 to -2000 go from 0 to 100 in brightness
+			// -1000 -> 0 brightness
+			// -2000 -> 1 brightness
+			uint8_t brightness = (x_axis + 1000)/-10;
+			pwm_driver_set(GREEN, brightness);
+			pwm_driver_set(RED, 0);
+		} else if (x_axis > 1000){
+			// 1000 to 2000 go from 0 to 100 in brightness
+			// 1000 -> 0 brightness
+			// 2000 -> 100 brightness
+			uint8_t brightness = (x_axis - 1000)/10;
+			pwm_driver_set(RED, brightness);
+			pwm_driver_set(GREEN, 0);
+		} else {
+			pwm_driver_set(RED, 0);
+			pwm_driver_set(GREEN, 0);
+		}
+	};
 }
